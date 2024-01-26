2a74c542cb6d44d1421b22fb918ab3b73eb1f7f4
Lecer <xarshiapro@gmail.com>
2024-01-26 23:16:43 +0330

    init files

---
+++ src/main/java/dev/lecer/mystic/mysticlava/mysticlava/core/MysticLava.java
@@ -0,0 +1,17 @@
+package dev.lecer.mystic.mysticlava.mysticlava.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MysticLava extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
