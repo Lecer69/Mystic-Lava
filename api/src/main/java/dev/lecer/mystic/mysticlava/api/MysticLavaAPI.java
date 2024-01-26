50cbe8c372b41c11febb3c581299559837b67580
Lecer <xarshiapro@gmail.com>
2024-01-26 23:48:12 +0330

    added multi module to project

---
+++ api/src/main/java/dev/lecer/mystic/mysticlava/api/MysticLavaAPI.java
@@ -0,0 +1,4 @@
+package dev.lecer.mystic.mysticlava.api;
+
+public interface MysticLavaAPI {
+}
--- core/src/main/java/dev/lecer/mystic/mysticlava/core/Core.java
+++ core/src/main/java/dev/lecer/mystic/mysticlava/core/Core.java
@@ -1,8 +1,8 @@
-package dev.lecer.mystic.mysticlava.mysticlava.core;
+package dev.lecer.mystic.mysticlava.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MysticLava extends JavaPlugin {
+public final class Core extends JavaPlugin {
 
     @Override
     public void onEnable() {
