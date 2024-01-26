497bbed1ba8f2a264a4ab8dceacb78aeeec7fa94
Lecer <xarshiapro@gmail.com>
2024-01-27 00:13:21 +0330

    setup plugin.yml

--- core/src/main/java/dev/lecer/mystic/mysticlava/core/MysticLava.java
+++ core/src/main/java/dev/lecer/mystic/mysticlava/core/MysticLava.java
@@ -2,7 +2,7 @@ package dev.lecer.mystic.mysticlava.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Core extends JavaPlugin {
+public final class MysticLava extends JavaPlugin {
 
     @Override
     public void onEnable() {
