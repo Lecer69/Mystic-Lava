a0894ffe3ffc5129aac9c5704924ca64b8879d05
Lecer <xarshiapro@gmail.com>
2024-02-01 01:46:03 +0330

    cleaned up Version.java

--- core/src/main/java/dev/lecer/mystic/mysticlava/core/enums/Version.java
+++ core/src/main/java/dev/lecer/mystic/mysticlava/core/enums/Version.java
@@ -12,33 +12,31 @@ import java.util.Arrays;
 @Getter
 @RequiredArgsConstructor
 public enum Version {
-    v1_8_R1(8),
-    v1_8_R2(8),
-    v1_8_R3(8),
-    v1_9_R1(9),
-    v1_9_R2(9),
-    v1_10_R1(10),
-    v1_11_R1(11),
-    v1_12_R1(12),
-    v1_13_R1(13),
-    v1_13_R2(13),
-    v1_14_R1(14),
-    v1_15_R1(15),
-    v1_16_R1(16),
-    v1_16_R2(16),
-    v1_16_R3(16),
-    v1_17_R1(17),
-    v1_18_R1(18),
-    v1_18_R2(18),
-    v1_19_R1(19),
-    v1_19_R2(19),
-    v1_19_R3(19),
-    v1_20_R1(20),
-    v1_20_R2(20),
-    v1_20_R3(20);
+    v1_8_R1,
+    v1_8_R2,
+    v1_8_R3,
+    v1_9_R1,
+    v1_9_R2,
+    v1_10_R1,
+    v1_11_R1,
+    v1_12_R1,
+    v1_13_R1,
+    v1_13_R2,
+    v1_14_R1,
+    v1_15_R1,
+    v1_16_R1,
+    v1_16_R2,
+    v1_16_R3,
+    v1_17_R1,
+    v1_18_R1,
+    v1_18_R2,
+    v1_19_R1,
+    v1_19_R2,
+    v1_19_R3,
+    v1_20_R1,
+    v1_20_R2,
+    v1_20_R3;
 
-    private final int id;
-    
     private static Version CURRENT;
     private static String STRING_VERSION;
 
